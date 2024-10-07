720c755adbe831cacfa6500c22a9f41348ec7367
Hayle_HB <haylemesekelhaylemariam@gmail.com>
2024-10-07 21:49:00 +0300

    noting to change except some comments

--- models/Subscribers.js
+++ models/Subscribers.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
-// Define the subscribers schema
 const subscriberSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Email is required"],
-    unique: true, // Ensure the email is unique
-    lowercase: true, // Store emails in lowercase
-    trim: true, // Remove spaces before and after the email
+    unique: true, 
+    lowercase: true, 
+    trim: true, 
     validate: {
       validator: function (v) {
-        // Regular expression to validate email format
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
       },
       message: (props) => `${props.value} is not a valid email!`,
